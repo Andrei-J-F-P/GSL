b8ae199d798a36cc82de5865e30140d85bfd4ab8
Andrei-J-F-P <an.ja.fe.po@gmail.com>
2021-11-22 11:35:42 +0800

    Added .groovy function for calling env variables

--- vars/environment.groovy
+++
@@ -1,4 +0,0 @@
-def call() {
-  sh 'KEY_VALUE_STR="string env variable"'
-  sh 'KEY_VALUE_INT=1234'
-}
---
+++ vars/loadEnvironmentVariables.groovy
@@ -0,0 +1,8 @@
+def call(path){
+    def props = readProperties  file: path
+    keys= props.keySet()
+    for(key in keys) {
+        value = props["${key}"]
+        env."${key}" = "${value}"
+    }
+}
