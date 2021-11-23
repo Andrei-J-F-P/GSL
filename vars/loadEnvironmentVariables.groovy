e64be04c54252eb242f084ace277067e06811694
Andrei-J-F-P <an.ja.fe.po@gmail.com>
2021-11-23 12:39:16 +0800

    Removed unused function

--- vars/loadEnvironmentVariables.groovy
+++
@@ -1,8 +0,0 @@
-def call(path){
-    def props = readProperties  file: path
-    keys= props.keySet()
-    for(key in keys) {
-        value = props["${key}"]
-        env."${key}" = "${value}"
-    }
-}
